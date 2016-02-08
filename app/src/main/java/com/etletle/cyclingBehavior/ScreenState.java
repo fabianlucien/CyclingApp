ff9ed8e872eb5986268deaf81917c8d6a69afb6a
fabianlucien <bongosart@gmail.com>
2016-02-08 08:57:55 +0100

    Revert "Thread update"
    This reverts commit b1b98ec92798ac55b4215fbbbf543d2119998cc0.

--- app/src/main/java/com/etletle/cyclingBehavior/ScreenState.java
+++ app/src/main/java/com/etletle/cyclingBehavior/ScreenState.java
@@ -1,25 +1,25 @@
-package com.etletle.cyclingBehavior;
-
-import android.os.PowerManager;
-
-/**
- * Created by FabianLucien on 1/19/16.
- */
-public class ScreenState {
-
-    private static boolean screenOn;
-
-    public static boolean returnScreenState(PowerManager pm){
-        // With context, you get global information about an application's environment
-        // getSystemService retrieves information from a class
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT_WATCH) {
-            screenOn = pm.isInteractive();
-
-        } else {
-            screenOn = pm.isScreenOn();
-        }
-
-        return screenOn;
-    }
-}
+//package com.etletle.cyclingBehavior;
+//
+//import android.os.PowerManager;
+//
+///**
+// * Created by FabianLucien on 1/19/16.
+// */
+//public class ScreenState {
+//
+//    public boolean screenOn;
+//
+//    public boolean returnScreenState(PowerManager pm){
+//        // With context, you get global information about an application's environment
+//        // getSystemService retrieves information from a class
+//
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT_WATCH) {
+//            screenOn = pm.isInteractive();
+//
+//        } else {
+//            screenOn = pm.isScreenOn();
+//        }
+//
+//        return screenOn;
+//    }
+//}
